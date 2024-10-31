8abf1455a013a993c0701e41965e649c9c2c0ecc
Germuu <jeremyyamajako@gmail.com>
2024-10-31 04:13:06 +0200

    testit tehty

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,32 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_virheellinen_t_nollataan(self):
+        self.varasto = Varasto(-5)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+     
+    def test_virheellinen_s_nollataan(self):
+        self.varasto = Varasto(10, -7)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_virheellinen_lisays(self):
+        self.varasto.lisaa_varastoon(-4)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_enemman_kuin_mahtuu(self):
+        self.varasto.lisaa_varastoon(20)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_virheellinen_otto(self):
+        self.varasto.lisaa_varastoon(10)
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-4), 0)
+    
+    def test_liian_suuri_otto(self):
+        self.varasto.lisaa_varastoon(3)
+        self.varasto.ota_varastosta(8)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+        
+    def test_tulostus(self):
+        teksti = f"saldo = {0}, vielä tilaa {10}"
+        self.assertEqual(str(self.varasto), teksti)
